51b8f027e8f11c65df3d5c133d95be66c147e79d
turarization <turar5216@gmail.com>
2024-08-10 08:17:21 +0500

    Add files via upload

---
+++ ChartExamples.py
@@ -0,0 +1,12 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+
+
+# Запуск цикла mainloop
+window.mainloop()
